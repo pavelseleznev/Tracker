5af2e3a0c69584cbacbc07fd4220b4cd12361fed
PavelSeleznev <seleznevpm@icloud.com>
2025-03-23 06:37:20 +0300

    Initial Commit

---
+++ Tracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tracker
+//
+//  Created by Pavel Seleznev on 3/23/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
